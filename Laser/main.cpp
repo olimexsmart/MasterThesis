c4965064d87927a162f90dbda27942fb8a5d9e77
Luca Olivieri <gliolivieri@live.it>
2018-04-11 16:26:46 +0200

    Test main for laser

--- Laser/main.cpp
+++ Laser/main.cpp
@@ -1,7 +1,52 @@
-#include <cstdio>
-#include "SerialDevice.h"
+#include "sicks3000.h"
+//#include "SerialDevice.h"
 #include <stdlib.h>
+#define WAIT 50
 
+int main() {
+	char TXbuffer[100] = "10000";
+	char RXbuffer[10000];
+	int baudrate = 500000;
+	const char mode[] = { '8','N','1',0 };
+	char portname[10] = "COM10";
+	int bytesRead;
+
+	// Decomment if needed to change default values
+	//printf("Port name:\t");
+	//scanf("%s", portname);
+	//printf("Baudrate:\t");
+	//scanf("%d", &baudrate);
+
+	// Struct to hold data from the laser scanner
+	SickS3000::LaserData ld;
+	// Laser instance
+	SickS3000 laser(portname, baudrate, mode);
+
+	while (true) {
+		if (laser.ReadLaser(ld)) {
+			// Print the data
+			printf("%u\n", ld.timestamp);
+			int i = 0;
+			for (auto const & r : ld.ranges) {
+				i++;
+				if (i % 10 == 0)
+					printf("\n");
+
+				printf("%.2f\t", r);
+			}
+		}
+		else
+			break;
+
+		Sleep(WAIT);
+	}
+
+	return 0;
+}
+
+
+
+/* // Main to test serial port with an Arduino and prime number test sketch
 int main(void) {
 	char TXbuffer[100] = "10000";
 	char RXbuffer[10000];
@@ -10,12 +55,12 @@ int main(void) {
 	char portname[10] = "COM7";
 	int bytesRead = 1;
 	char c;
-	/*
-	printf("Port name:\t");
-	scanf("%s", portname);
-	printf("Baudrate:\t");
-	scanf("%d", &baudrate);
-	*/
+
+	//printf("Port name:\t");
+	//scanf("%s", portname);
+	//printf("Baudrate:\t");
+	//scanf("%d", &baudrate);
+
 
 	SerialDevice sd(portname, baudrate, mode);
 
@@ -42,3 +87,4 @@ int main(void) {
 
 	return 0;
 }
+*/
